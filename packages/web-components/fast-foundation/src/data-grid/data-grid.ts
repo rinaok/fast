3b76acd5941271c8811a09ed94c01e8514441eae
Stephane Comeau <scomea@microsoft.com>
2022-04-14 13:57:39 -0700

    tweak

--- packages/web-components/fast-foundation/src/data-grid/data-grid.ts
+++ packages/web-components/fast-foundation/src/data-grid/data-grid.ts
@@ -369,7 +369,7 @@ export class FASTDataGrid extends FASTElement {
     /**
      * Children that are rows
      *
-     * @public
+     * @internal
      */
     @observable
     public rowElements: HTMLElement[];
