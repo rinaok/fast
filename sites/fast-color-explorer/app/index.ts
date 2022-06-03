515338552392a5b58346801dfd63e2fd9604b12c
Brian Heston <47367562+bheston@users.noreply.github.com>
2022-06-03 10:50:30 -0700

    Color explorer: Added banner to direct to Fluent UI version

--- sites/fast-color-explorer/app/index.ts
+++ sites/fast-color-explorer/app/index.ts
@@ -1,5 +1,27 @@
 import {
-    allComponents as fastComponents,
+    fastAnchor,
+    fastBadge,
+    fastButton,
+    fastCard,
+    fastCheckbox,
+    fastDesignSystemProvider,
+    fastDivider,
+    fastFlipper,
+    fastListbox,
+    fastMenu,
+    fastMenuItem,
+    fastOption,
+    fastProgress,
+    fastRadio,
+    fastRadioGroup,
+    fastSlider,
+    fastSwitch,
+    fastTab,
+    fastTabPanel,
+    fastTabs,
+    fastTextField,
+    fastTreeItem,
+    fastTreeView,
     provideFASTDesignSystem,
 } from "@microsoft/fast-components";
 import { fastToolingColorPicker } from "../app/components/color-picker";
@@ -9,7 +31,31 @@ import { appComponents } from "./custom-elements";
 import "./base.css";
 
 provideFASTDesignSystem()
-    .register(fastComponents)
+    .register(
+        fastAnchor(),
+        fastBadge(),
+        fastButton(),
+        fastCard(),
+        fastCheckbox(),
+        fastDesignSystemProvider(),
+        fastDivider(),
+        fastFlipper(),
+        fastListbox(),
+        fastMenu(),
+        fastMenuItem(),
+        fastOption(),
+        fastProgress(),
+        fastRadio(),
+        fastRadioGroup(),
+        fastSlider(),
+        fastSwitch(),
+        fastTab(),
+        fastTabPanel(),
+        fastTabs(),
+        fastTextField(),
+        fastTreeItem(),
+        fastTreeView()
+    )
     .withPrefix("fast-tooling")
     .register(fastToolingColorPicker())
     .withPrefix("app")
